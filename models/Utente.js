b1074a471a7eff28b8b0d0c02615ca488fa9b738
antonio di nocera <antonio.din74@gmail.com>
2023-05-16 14:57:56 +0200

    prima release

---
+++ models/Utente.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const utenteschema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    cognome: {
+        type: String,
+        required: true
+    },
+    indirizzo: {
+        type: String,
+    },
+    citta: {
+        type: String,
+    },
+    cap: {
+        type: String,
+    },
+    piva: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        default: ''
+    }
+})
+
+let Utente = mongoose.model('Utente', utenteschema, 'utenti')
+
+module.exports = Utente
